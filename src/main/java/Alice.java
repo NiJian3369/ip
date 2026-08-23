b92157fbce539f83a22b3752590049188bae7042
NI JIAN <nijian3369@gmail.com>
2026-08-24 01:56:06 +0800

    complete level 0, rename Duke to Alice, added greeting and exit

---
+++ src/main/java/Alice.java
@@ -0,0 +1,10 @@
+public class Alice {
+    public static void main(String[] args) {
+        String banner = "________________________________________\n"
+                + "Good day mate! Alice here! What can I do for ya today?\n"
+                + "\n________________________________________\n"
+                + "Good bye mate, I'm off! See ya around!\n"
+                + "\n________________________________________\n";
+        System.out.println(banner);
+    }
+}
