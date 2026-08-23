42bc1c4588a5a3e51ace8c7a15c6af51a37fab80
NI JIAN <nijian3369@gmail.com>
2026-08-24 02:55:04 +0800

    Complete level-2, stores input and prints input history when input list

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -4,6 +4,8 @@ public class Alice {
     public static void main(String[] args) {
         System.out.println("Good day mate! Alice here! What can I do for ya today?");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         String input;
 
@@ -13,8 +15,14 @@ public class Alice {
             if (input.equals("bye")) {
                 System.out.println("Good bye mate, I'm off! See ya around!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
             } else {
-                System.out.println(input);
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
         }
     }
