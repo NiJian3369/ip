59fd0030eff8c9b21fc1bcd7f73a131d2c0df956
NI JIAN <nijian3369@gmail.com>
2026-08-24 02:43:54 +0800

    Complete level-1, echoes user input and exit with bye

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Alice {
     public static void main(String[] args) {
-        String banner = "________________________________________\n"
-                + "Good day mate! Alice here! What can I do for ya today?\n"
-                + "\n________________________________________\n"
-                + "Good bye mate, I'm off! See ya around!\n"
-                + "\n________________________________________\n";
-        System.out.println(banner);
+        System.out.println("Good day mate! Alice here! What can I do for ya today?");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Good bye mate, I'm off! See ya around!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
